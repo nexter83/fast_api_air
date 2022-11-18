ccfa4fc847ff8acd93778e3c33733740fd381089
Oleg Basmanov <basmanovos@pik.ru>
2022-11-18 20:58:13 +0300

    add docker

--- src/postgres_air/main.py
+++ src/postgres_air/main.py
@@ -2,4 +2,4 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run('postgres_air.app:app', host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run('postgres_air.app:app', host="0.0.0.0", port=8000)
