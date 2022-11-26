ebda6901f0b9ce14ea9e73a8bea1024a2a2bf2f4
Oleg Basmanov <basmanovos@pik.ru>
2022-11-26 10:04:32 +0300

    add fastapi-pagination

--- src/postgres_air/api/accounts.py
+++ src/postgres_air/api/accounts.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, Depends, status, Response
 
+from fastapi_pagination import LimitOffsetPage, add_pagination
 from ..services.accounts import AccountServices
 from ..schemas.accounts import (
-    ListAccountSchema,
+    AccountOutSchema,
     AccountResponse,
     CreateAccountSchema,
     UpdateAccountSchema,
 )
 from ..services.auth import RoleChecker
 
-router = APIRouter(prefix="/api/accounts", tags=['accounts'])
+router = APIRouter(prefix="/api/accounts", tags=["accounts"])
 
 ok = "ok"
 allow_create_resource = RoleChecker(["admin"])
@@ -19,39 +20,22 @@ allow_read_resource = RoleChecker(["admin", "user"])
 @router.get(
     "/",
     status_code=status.HTTP_200_OK,
-    response_model=ListAccountSchema,
-    dependencies=[Depends(allow_read_resource)]
+    response_model=LimitOffsetPage[AccountOutSchema],
+    dependencies=[Depends(allow_read_resource)],
 )
 def get_accounts(
-    service: AccountServices = Depends(),
-    page: int = 0,
-    page_size: int = None,
-    order_by=None,
-    is_desc: bool = False,
-) -> dict:
-    res, total, out_page_size, count = service.get_accounts(
-     page, page_size, order_by, is_desc
-    )
-    return {
-        "status": ok,
-        "total": total,
-        "count": count,
-        "page": page,
-        "page_size": page_size or out_page_size,
-        "accounts": res,
-    }
+    service: AccountServices = Depends(), order_by=None, is_desc: bool = False
+):
+    return service.get_accounts(order_by, is_desc)
 
 
 @router.get(
     "/{account_id}",
     status_code=status.HTTP_200_OK,
     response_model=AccountResponse,
-    dependencies=[Depends(allow_read_resource)]
+    dependencies=[Depends(allow_read_resource)],
 )
-def get_account(
-        account_id: str,
-        service: AccountServices = Depends()
-) -> dict:
+def get_account(account_id: str, service: AccountServices = Depends()) -> dict:
     res = service.get_account(account_id)
     return {"status": ok, "account": res}
 
@@ -60,11 +44,10 @@ def get_account(
     "/",
     status_code=status.HTTP_201_CREATED,
     response_model=AccountResponse,
-    dependencies=[Depends(allow_create_resource)]
+    dependencies=[Depends(allow_create_resource)],
 )
 def create_account(
-    account: CreateAccountSchema,
-    service: AccountServices = Depends()
+    account: CreateAccountSchema, service: AccountServices = Depends()
 ) -> dict:
     res = service.create_account(account)
     return {"status": ok, "account": res}
@@ -73,23 +56,18 @@ def create_account(
 @router.put(
     "/{account_id}",
     response_model=AccountResponse,
-    dependencies=[Depends(allow_create_resource)]
+    dependencies=[Depends(allow_create_resource)],
 )
 def update_account(
-    account_id: str,
-        account: UpdateAccountSchema,
-        service: AccountServices = Depends()
+    account_id: str, account: UpdateAccountSchema, service: AccountServices = Depends()
 ) -> dict:
     res = service.update_account(account_id, account)
     return {"status": ok, "account": res}
 
 
-@router.delete(
-    "/{account_id}",
-    dependencies=[Depends(allow_create_resource)]
-)
-def delete_account(
-        account_id: str,
-        service: AccountServices = Depends()
-) -> Response:
+@router.delete("/{account_id}", dependencies=[Depends(allow_create_resource)])
+def delete_account(account_id: str, service: AccountServices = Depends()) -> Response:
     return service.delete_account(account_id)
+
+
+add_pagination(router)
--- src/postgres_air/services/accounts.py
+++ src/postgres_air/services/accounts.py
@@ -1,18 +1,15 @@
 from datetime import datetime
 
 from fastapi import Depends, HTTPException, status, Response
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
-
-from ..services import create_query
+from fastapi_pagination import paginate
 from ..database import get_session
 from ..models.accounts import Account
 
 
 class AccountServices:
-    def __init__(
-            self,
-            session: Session = Depends(get_session),
-    ):
+    def __init__(self, session: Session = Depends(get_session)):
         self.session = session
 
     def _get_account(self, account_id):
@@ -21,22 +18,11 @@ class AccountServices:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
         return query
 
-    def get_accounts(
-            self,
-            page: int = 0,
-            page_size: int = None,
-            order_by=None,
-            is_desc: bool = False
-    ):
-        query, total, page_size = create_query(
-            session=self.session,
-            model=Account,
-            page=page,
-            page_size=page_size,
-            order_by=order_by,
-            is_desc=is_desc,
-        )
-        return query.all(), total, page_size, query.count()
+    def get_accounts(self, order_by=None, is_desc: bool = False):
+        query = self.session.query(Account)
+        if order_by:
+            query = query.order_by(desc(order_by)) if is_desc else query.order_by(order_by)
+        return paginate(query.all())
 
     def get_account(self, account_id):
         res = self._get_account(account_id)
@@ -53,7 +39,7 @@ class AccountServices:
         account_query = self._get_account(account_id)
         if not account_query:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-        account.update_ts = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
+        account.update_ts = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S.%f")
         account_query.update(account.dict(exclude_none=True))
         self.session.commit()
         return account_query.first()
@@ -64,8 +50,10 @@ class AccountServices:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
         account_m = account_query.first()
         if not account_m:
-            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                detail=f'No account with this id: {account_id} found')
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"No account with this id: {account_id} found",
+            )
         account_query.delete()
         self.session.commit()
         return Response(status_code=status.HTTP_204_NO_CONTENT)
